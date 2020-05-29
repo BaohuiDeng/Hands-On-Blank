1f310753d97988ba7426a5e57ab7af43de077a0b
BaohuiDeng <46564403+BaohuiDeng@users.noreply.github.com>
2020-05-29 23:16:47 +0200

    Update document.py

--- document.py
+++ document.py
@@ -1,8 +1,8 @@
 import mongoengine
 from flask import Flask 
 from mongoengine import *
-#
-class User(Document):
-    username = StringField(required=True, max_length=30)
-    email = StringField(required=True, max_length=50)
-    password = StringField(required=True)
+
+
+#Task 2.1 start
+
+#Task2.1 end
