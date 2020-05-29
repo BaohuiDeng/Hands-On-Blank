cec05cc97f5686dd26744196852d418467b83614
BaohuiDeng <46564403+BaohuiDeng@users.noreply.github.com>
2020-05-29 23:14:08 +0200

    Update forms.py

--- forms.py
+++ forms.py
@@ -2,14 +2,9 @@ from flask_wtf import Form
 from wtforms import TextField, StringField, PasswordField, SubmitField
 
 
-#
-class RegisterForm(Form):
-    
-    username = StringField('Username')
-    email = StringField('Email')
-    password = PasswordField('Password')
-    submit = SubmitField('Register')
+#Task 1.1 start
 
+# Task 1.1 end
 
 
 
